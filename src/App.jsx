c930a8e73d3be2e8bfcff005f6651ab3016b6f5e
trenthendricks2 <trenthendricks2@yahoo.com>
2024-02-23 14:48:34 -0600

    Innitial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+   <main className='min-h-screen flex flex-col bg-gradient-to-r from-slate-800 to-slate-900 text-white text-sm sm:text-base'>
+    <div>his</div>
+   </main>
+  )
+}
+
+export default App
