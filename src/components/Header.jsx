1bc5aab69b84709a3e74b33fb1f9efbc1058cb95
trenthendricks2 <trenthendricks2@yahoo.com>
2024-02-29 13:44:56 -0600

    Created ClassDropDown component to clean up header and removed repeated code.
    also created archetype util for link etc.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faMagnifyingGlass, faChartColumn, faAngleRight, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import { useState, useId, } from 'react'
+import ClassDropdown from './ClassDropdown'
 
 
 export default function Header(props) {
@@ -71,7 +72,7 @@ export default function Header(props) {
   {/* Drop down menus */}
       <div className='flex' >
 
-        {/* Raid dropdown */}
+    {/* Raid dropdown */}
         <div className='relative inline-block text-left'
              onMouseLeave={()=>{ 
               if(raidDropShow===true) {
@@ -90,7 +91,7 @@ export default function Header(props) {
             </button>
           </div>
           { raidDropShow===true ? 
-              <div className=" absolute z-10 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bg-gray-800 text-gray-400" 
+              <div className=" absolute z-10 mt-2 w-40 origin-top-right divide-y divide-gray-500 rounded-md bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-gray-400" 
                    role="menu" 
                    aria-orientation="vertical" 
                    aria-labelledby="menu-button" 
@@ -114,43 +115,12 @@ export default function Header(props) {
           }
         </div>
 
-        {/* Classes Dropdown */}
-        <div className='relative inline-block text-left pl-5'
-             onMouseLeave={()=>{ 
-              if(classDropShow===true) {
-                document.addEventListener("click", classListener)}}
-              }
-        >
-          <div className=''>
-            <button type="button"
-                    id="menu-button"
-                    aria-expanded="true"
-                    aria-haspopup="true"
-                    onClick={()=>{setClassDropShow(!classDropShow)}}
-                    className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 rounded-md bg-gray-800 hover:bg-gray-700 text-sm font-semibold text-gray-400">
-              Classes
-              <FontAwesomeIcon icon={faAngleDown} className='relative left-1 top-1'/>
-            </button>
-          </div>
-          { classDropShow===true ? 
-              <div className=" absolute z-10 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bg-gray-800 text-gray-400" 
-                   role="menu" 
-                   aria-orientation="vertical" 
-                   aria-labelledby="menu-button" 
-                   tabindex="-1">
-                <div className="py-1" role="none">
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Warrior <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-2">Mages <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-3">Fighters <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-4">Gunners <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-5">Assassins <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-6">Specialists <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                </div>
-                
-              </div> 
-            : ""
-          }
-        </div>
+    {/* Classes Dropdown */}
+        <ClassDropdown 
+          listener={classListener}
+          dropShow={classDropShow}
+          setDropShow={setClassDropShow}
+        />
 
         {/* Raidguide dropdown */}
         <div className='relative inline-block text-left pl-5'
@@ -171,7 +141,7 @@ export default function Header(props) {
             </button>
           </div>
           { raidGuideDropShow===true ? 
-            <div className=" absolute z-10 mt-2 w-40 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bg-gray-800 text-gray-400" 
+            <div className=" absolute z-10 mt-2 w-40 origin-top-right divide-y divide-gray-500 rounded-md bg-gray-800 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none text-gray-400" 
               role="menu" 
               aria-orientation="vertical" 
               aria-labelledby="menu-button" 
@@ -196,42 +166,11 @@ export default function Header(props) {
         </div>
 
         {/* class guide dropdown */}
-        <div className='relative inline-block text-left pl-5'
-             onMouseLeave={()=>{ 
-              if(classGuideDropShow===true) {
-                document.addEventListener("click", classGuideListener)}}
-              }
-        >
-          <div className=''>
-            <button type="button"
-                    id="menu-button"
-                    aria-expanded="true"
-                    aria-haspopup="true"
-                    onClick={()=>{setClassGuideDropShow(!classGuideDropShow)}}
-                    className="inline-flex w-full justify-center gap-x-1.5 px-3 py-2 rounded-md bg-gray-800 hover:bg-gray-700 text-sm font-semibold text-gray-400">
-              Class Guides
-              <FontAwesomeIcon icon={faAngleDown} className='relative left-1 top-1'/>
-            </button>
-          </div>
-          { classGuideDropShow===true ? 
-              <div className=" absolute z-10 mt-2 w-30 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none bg-gray-800 text-gray-400" 
-                   role="menu" 
-                   aria-orientation="vertical" 
-                   aria-labelledby="menu-button" 
-                   tabindex="-1">
-                <div className="py-1" role="none">
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Warrior <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Mages <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Fighters <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Gunners <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Assassins <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                  <a href="#" className=" block px-4 py-2 text-sm hover:bg-gray-700" role="menuitem" tabindex="-1" id="menu-item-1">Specialists <FontAwesomeIcon icon={faAngleRight} className='absolute right-3' /></a>
-                </div>
-                
-              </div> 
-            : ""
-          }
-        </div>
+        <ClassDropdown 
+          listener={classGuideListener}
+          dropShow={classGuideDropShow}
+          setDropShow={setClassGuideDropShow}
+        />
       </div>
         
     
---
+++ src/utils/archetype.js
@@ -0,0 +1,100 @@
+export const archetype = {
+    Warriors: {
+      Berserker: {
+        Guide: "https://docs.google.com/document/d/1QpdFMV-y0tEyxg4GlDxwKprD9li3sgEV3fnSyH_kCqk/edit#heading=h.m6rcokaux2q",
+        Logs: "#"
+      },
+      Destroyer: {
+        Guide: "https://docs.google.com/document/d/1lh3uX7-3n6rI7uiMFk032dot1fAIu5DVxQKz99X9vX0/edit#heading=h.m6rcokaux2q",
+        Logs: ""
+      },
+      Gunlancer: {
+        Guide: "https://docs.google.com/document/d/1EPVvtqL_5URvU6H6RP89pWi_0qmo7RJJAsqA4rGuGac/edit?usp=sharing",
+        Logs: ""
+      },
+      Paladin: {
+        Guide: "https://docs.google.com/document/d/13q48wuWMMJ8bTJHf1ro_xOECS_OOzaEv0y47msJxt4g/edit",
+        Logs: ""
+      },
+      Slayer: {
+        Guide: "https://docs.google.com/document/d/1hZuP7TRwvaNDBnt7QKZJB-TdJLKvgtRdxhQr104l5Qg/edit#heading=h.m6rcokaux2q",
+        Logs: ""
+      },
+    },
+    Mages: {
+      Arcanist: {
+        Guide: "https://docs.google.com/document/d/1MWWgdrTIJBj0WXmAqE4DspVobgMlDx4aaI7ZwCj1-AM/edit#heading=h.m6rcokaux2q",
+        Logs: "#"
+      },
+      Bard: {
+        Guide: "https://docs.google.com/document/d/1hgR9_xMPaB95NUPgEx0hwgE9vjAQQCeVK6S74ZDnR58/edit",
+        Logs: ""
+      },
+      Sorceress: {
+        Guide: "https://docs.google.com/document/d/1Nmia4tXicFJb6aiErXfFDeRSI9NAthvU23A8d038_m4/edit#heading=h.m6rcokaux2q",
+        Logs: ""
+      },
+      Summoner: {
+        Guide: "https://docs.google.com/document/d/1OePqvZocw3FYPphmUGQpzExeHPK1J7TBncij3w4cyHc/edit#heading=h.m6rcokaux2q",
+        Logs: ""
+      }
+    },
+    Fighters: {
+        Galvier: {
+          Guide: "https://docs.google.com/document/d/1X6zj1AJBUiYpADNKCIaeJsQYT1IZc8V_2YeXFz-KuYc/edit",
+          Logs: "#"
+        },
+        Scrapper: {
+          Guide: "https://docs.google.com/document/d/1AQ300Di03divMVbRimnp0LmFN7VBQMSBqQaXZP2sDEo/edit#heading=h.ek6cx7o5b8v4",
+          Logs: ""
+        },
+        Soulfist: {
+          Guide: "https://docs.google.com/document/d/12IcR8HrywQQhhThnGZxz-Gsq4KryrT5aKggGcgkX9bA/edit#heading=h.m6rcokaux2q",
+          Logs: ""
+        },
+        Wardancer: {
+          Guide: "https://docs.google.com/document/d/1EQD2Rt7XTDBDJMMz2zFK9Swcc1io4ycbicbEwauUaFg/edit#heading=h.m6rcokaux2q",
+          Logs: ""
+        },
+        Striker: {
+            Guide: "https://maxroll.gg/lost-ark/build-guides/death-blow-striker-raid-guide",
+            Logs: ""
+          },
+        Breaker: {
+        Guide: "https://docs.google.com/document/d/1OJQfnS3yd8sxNLI8gpxmrzW5fLoTt0Rl8AuP2adw9RU/edit#heading=h.m6rcokaux2q",
+        Logs: ""
+        },
+      },
+    Gunners: {
+        Artillerist: {
+          Guide: "https://docs.google.com/document/d/1tCijlx-0S31CBUS_at69RBtzYwu-2rMfXT2A6TbJzh8/edit",
+          Logs: "#"
+        },
+        Deadeye: {
+          Guide: "https://docs.google.com/document/d/11UgBa19HP7-BZv09tNJTBTIbvx7oQvSWFcBsGGIUXd8/edit#heading=h.m6rcokaux2q",
+          Logs: ""
+        },
+        Gunslinger: {
+          Guide: "https://docs.google.com/document/d/1Dr4egW6_YR7JGkukK-pbPXg3ckbfE31iuMxOSSudD64/edit#heading=h.m6rcokaux2q",
+          Logs: ""
+        },
+        Machinist: {
+          Guide: "https://docs.google.com/document/d/1Nkd1R14ZNVHsyfHCiyw_nd2stA-5PDseNM58dC5gVK0/edit#heading=h.m6rcokaux2q",
+          Logs: ""
+        },
+        Sharpshooter: {
+            Guide: "https://docs.google.com/document/d/1K3l9Uvgcb6QpJ-A5qccGEhhMRfps8FmU2Ean-lbC5sc/edit",
+            Logs: ""
+          },
+      },
+    Specialists: {
+        Aeromancer: {
+            Guide: "https://docs.google.com/document/d/1AiPHCB1oa56fM-l0B8ao-11-JYAdiA3qFtS339Dg3dQ/edit",
+            Logs: ""
+        },
+        Artist: {
+            Guide: "https://docs.google.com/document/d/1OlCBWkiQRt0odr6BoOotxTpzhDcoEAicv8vJdzJIViI/edit#heading=h.m6rcokaux2q",
+            Logs: ""
+        }
+    }
+  }
