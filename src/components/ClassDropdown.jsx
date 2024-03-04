85edbbd692ce1774dd48a3ade3d43a6dfca73f52
trenthendricks2 <trenthendricks2@yahoo.com>
2024-03-04 15:31:10 -0600

    Edited  and styled subclassdropdown

--- src/components/ClassDropdown.jsx
+++ src/components/ClassDropdown.jsx
@@ -54,7 +54,12 @@ export default function Dropdown(props) {
           <div key={name} 
                className="" 
                onClick={() => {
+                if(SClassDropShow !== name) {
                 SubClassDropShow(name)
+                } else {
+                  setSClassDropShow('false')
+                }
+
                }}>
             <button className='inline-flex w-full justify-left px-3 py-2 pr-5 hover:bg-gray-700 text-sm hover:rounded-md' id={name + 'classDrop'}>{name}<FontAwesomeIcon icon={faAngleRight} className='absolute right-1'/></button>
           </div>
@@ -67,9 +72,14 @@ export default function Dropdown(props) {
         <div key={subClassName} className=''>
           {Object.keys(archetype.classes[subClassName]).map((name) => {
             return (
-              <div>
-                {name}
+              <div className='mt-1 rounded-md bg-gray-800 '>
+                <div key={name} >
+                  <button id={name+' subClassDrop'} className='inline-flex w-full justify-left px-3 py-2 pr-5 hover:bg-gray-700 text-sm rounded-md text-gray-400 bg-gray-800 border-x border-sky-400'>
+                    {name}
+                  </button>
+                </div>  
               </div>
+              
             )
           })}
         </div>
@@ -93,7 +103,7 @@ export default function Dropdown(props) {
               <FontAwesomeIcon icon={faAngleDown} className='relative left-1 top-1'/>
             </button>
           </div>
-          <div className='flex'>
+          <div className='absolute space-x-[100px] '>
             { dropShow===true ? 
                 <div className=" absolute mt-1 w-24 origin-top-right rounded-md bg-gray-800 shadow-lg text-gray-400" >
                   
@@ -108,32 +118,32 @@ export default function Dropdown(props) {
               : ""
             }
             { SClassDropShow === 'Fighters' ? 
-              <div> 
+              <div key="fighters" className='mt-[114px]'> 
                 <SubClassDropDown subClassName="Fighters"/>
               </div>
               :
               SClassDropShow === 'Mages' ? 
-              <div> 
+              <div key="mages" className='mt-[77px] '> 
                 <SubClassDropDown subClassName="Mages"/>
               </div>
               :
               SClassDropShow === 'Warriors' ? 
-              <div> 
+              <div key="warriors" className=''> 
                 <SubClassDropDown subClassName="Warriors"/>
               </div>
               :
               SClassDropShow === 'Assassins' ? 
-              <div> 
+              <div key="assasins" className='mt-[42px]'>  
                 <SubClassDropDown subClassName="Assassins"/>
               </div>
               :
               SClassDropShow === 'Gunners' ? 
-              <div> 
+              <div key="gunners" className='mt-[152px]'> 
                 <SubClassDropDown subClassName="Gunners"/>
               </div>
               :
               SClassDropShow === 'Specialists' ? 
-              <div> 
+              <div key="specialists"className='mt-[184px]'> 
                 <SubClassDropDown subClassName="Specialists"/>
               </div>
               : ""  
