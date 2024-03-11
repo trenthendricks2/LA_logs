c6f373b81eff8f173884416b4ef176931b76c0f0
trenthendricks2 <trenthendricks2@yahoo.com>
2024-03-11 15:11:12 -0500

    begining to add server integration

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,22 @@
-import { useState } from 'react'
+import { useState, } from 'react'
+import axios from 'axios'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Body from './components/Body'
 
 function App() {
   const [playersearch, setPlayersearch] = useState()
+  
 
   function searchPlayer (playerName) {
-    
+    axios.post('http://localhost:5000/post/playerSearch', { 
+            "playerName": playerName
+          })
+          .then( response => {
+            console.log('Server Response:', response.data)
+          }).catch( error => {
+            console.error(error)
+          })
     setPlayersearch(playerName)
   }
 
